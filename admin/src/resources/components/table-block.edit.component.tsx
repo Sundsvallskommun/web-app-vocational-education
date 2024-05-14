a2be3a21146d604ee5be491fdba91d4db99dc88e
Tim Lind <tim.lind@sundsvall.se>
2024-05-14 10:55:38 +0200

    fix: changed filter-key from pageId to id

--- admin/src/resources/components/table-block.edit.component.tsx
+++ admin/src/resources/components/table-block.edit.component.tsx
@@ -3,10 +3,11 @@ import { TableBlockList } from '../table-block/table-block.list.component';
 
 export const EditTableBlock = (props: any) => {
   const record = useRecordContext();
+
   return (
     <div>
       <ReferenceManyField source="pageId" reference="tableBlock" target="pageId">
-        <TableBlockList filter={{ pageId: record.pageId }} pagination={false} actions={false} />
+        <TableBlockList filter={{ pageId: record.id }} pagination={false} actions={false} />
       </ReferenceManyField>
     </div>
   );

89008861283550da303b975acf23c1db8a313a1d
Tim Lind <tim.lind@sundsvall.se>
2024-09-12 10:25:44 +0200

    fix: migrations modified unecessary constraint removed

--- backend/prisma/migrations/20240910133632_sync_of_property_names/migration.sql
+++ backend/prisma/migrations/20240910133632_sync_of_property_names/migration.sql
@@ -3,7 +3,7 @@
 
   - You are about to drop the column `location` on the `User_SavedInterest` table. All the data in the column will be lost.
   - You are about to drop the column `type` on the `User_SavedInterest` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[category,level,studyLocation]` on the table `User_SavedInterest` will be added. If there are existing duplicate values, this will fail.
+  - MODIFIED: A unique constraint covering the columns `[category,type,location]` on the table `User_SavedInterest` will be removed.
 
 */
 -- DropIndex
@@ -14,6 +14,3 @@ ALTER TABLE `User_SavedInterest` DROP COLUMN `location`,
     DROP COLUMN `type`,
     ADD COLUMN `level` VARCHAR(191) NOT NULL DEFAULT '',
     ADD COLUMN `studyLocation` VARCHAR(191) NOT NULL DEFAULT '';
-
--- CreateIndex
-CREATE UNIQUE INDEX `User_SavedInterest_category_level_studyLocation_key` ON `User_SavedInterest`(`category`, `level`, `studyLocation`);
--- backend/prisma/migrations/20240912064707_removed_unnecessary_constraint/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX `User_SavedInterest_category_level_studyLocation_key` ON `User_SavedInterest`;

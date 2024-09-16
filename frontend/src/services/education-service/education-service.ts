b32ff548f1a4e46a3959041435c6980367cf823f
Tim Lind <tim.lind@sundsvall.se>
2024-09-16 09:11:07 +0200

    fix: overlooks after fetched filters update

--- frontend/src/services/education-service/education-service.ts
+++ frontend/src/services/education-service/education-service.ts
@@ -11,6 +11,7 @@ import { ValueOf } from '@utils/types';
 import { objToQueryString } from '@utils/url';
 import dayjs from 'dayjs';
 import { ApiResponse, apiService } from '../api-service';
+import { getFormattedLabelFromValue } from '@utils/labels';
 
 export const emptyEducationFilterOptions: EducationFilterOptions = {
   q: '',
@@ -54,11 +55,11 @@ export const getEducationFilterValueString = (filter, value) => {
     case 'sortFunction':
       return sortFilter.find((choice) => choice.value === value).label;
     case 'category':
-      return value.join(' | ');
+      return value.map((x) => getFormattedLabelFromValue(x)).join(' | ');
     case 'level':
-      return value.join(' | ');
+      return value.map((x) => getFormattedLabelFromValue(x)).join(' | ');
     case 'studyLocation':
-      return value.join(' | ');
+      return value.map((x) => getFormattedLabelFromValue(x)).join(' | ');
     case 'distance':
       return value === 'true' ? 'Ja' : 'Nej';
     case 'latestApplicationDate':
@@ -66,7 +67,7 @@ export const getEducationFilterValueString = (filter, value) => {
     case 'startDate':
       return value;
     case 'scope':
-      return value.map((x) => `${x}%`).join(' | ');
+      return value.map((x) => x.replace('.0', '%')).join(' | ');
     default:
       return '';
   }

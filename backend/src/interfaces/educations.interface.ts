32069cb5e3f24371327a3d8cb0244fc31cf323e5
Tim Lind <tim.lind@sundsvall.se>
2025-02-28 09:30:47 +0100

    fix: updated PageResponse type and pagemocks

--- backend/src/interfaces/educations.interface.ts
+++ backend/src/interfaces/educations.interface.ts
@@ -1,5 +1,26 @@
 import { PagedCoursesResponse } from '@/data-contracts/education-finder/data-contracts';
-import { Page, User_SavedInterest } from '@prisma/client';
+import {
+  ContactFormBlock,
+  ContactFormBlockEmails,
+  EditRolesOnPage,
+  EmployerPromotionsBlock,
+  EmployerPromotionsBlockPromotions,
+  FaqBlock,
+  FaqBlockQuestions,
+  ImportantDatesBlock,
+  ImportantDatesBlockDateCards,
+  LogosBlock,
+  LogosBlockLogos,
+  MapBlock,
+  Page,
+  PromotionsBlock,
+  PromotionsBlockPromotions,
+  TableBlock,
+  TableBlockCell,
+  TableBlockHeader,
+  TableBlockRow,
+  User_SavedInterest,
+} from '@prisma/client';
 
 export interface UserSavedInterestStatistics extends Omit<User_SavedInterest, 'userId' | 'studyLocation'> {
   studyLocation: string[];
@@ -11,7 +32,57 @@ export interface UserSavedInterestStatistics extends Omit<User_SavedInterest, 'u
   ended: number;
 }
 
-export type PageResponse = Page & { educationsStartingBlock?: PagedCoursesResponse };
+type PagePromotionsBlockPromotions = PromotionsBlockPromotions & {
+  promotedPage: Page;
+};
+
+type PagePromotionsBlock = PromotionsBlock & {
+  promotions: PagePromotionsBlockPromotions[];
+};
+
+type PageImportantDatesBlock = ImportantDatesBlock & {
+  dateCards: ImportantDatesBlockDateCards[];
+  referencedImportantDatesBlockPageUrl?: string;
+};
+
+type PageEmployerPromotionsBlock = EmployerPromotionsBlock & {
+  employerPromotions: EmployerPromotionsBlockPromotions[];
+};
+
+type PageFaqBlock = FaqBlock & {
+  questions: FaqBlockQuestions[];
+};
+
+type PageContactFormBlock = ContactFormBlock & {
+  emails: ContactFormBlockEmails[];
+};
+
+type PageLogosBlock = LogosBlock & {
+  logos: LogosBlockLogos[];
+};
+
+type PageTableBlockRow = TableBlockRow & {
+  cells: TableBlockCell[];
+};
+type PageTableBlock = TableBlock & {
+  rows: PageTableBlockRow[];
+  headers: TableBlockHeader[];
+  cells: TableBlockCell[];
+};
+
+export type PageResponse = Page & {
+  editRoles?: EditRolesOnPage[] | null;
+  promotionsBlock: PagePromotionsBlock[] | null;
+  promotedBy?: PromotionsBlockPromotions[] | null;
+  mapBlock: MapBlock[] | null;
+  employerPromotionsBlock: PageEmployerPromotionsBlock | null;
+  importantDatesBlock: PageImportantDatesBlock[] | null;
+  faqBlock: PageFaqBlock[] | null;
+  logosBlock: PageLogosBlock[] | null;
+  tableBlock: PageTableBlock[] | null;
+  contactFormBlock: PageContactFormBlock[] | null;
+  educationsStartingBlock?: PagedCoursesResponse | null;
+};
 
 export type GetEducationFilter = 'level' | 'scope' | 'studyLocation' | 'category';
 export type GetEducationFiltersResponseData = {
